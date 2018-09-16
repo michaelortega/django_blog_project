4b82f021d07567439d1fb02ad34ce531c5dcd001
Michael Ortega <michaelortega@Michaels-MacBook-Pro.local>
2018-09-16 01:46:05 -0700

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self): #The shows how the post are displayed in the shell on Post.objects.all()
+        return self.title
