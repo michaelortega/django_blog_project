5845bb73d00f6927f1a617ee49aa2d52f2ce3b7a
michaelortega <michaelortega93@gmail.com>
2018-09-17 14:11:02 -0700

    Ability to change account settings. Added User profile pictures to home page. Auto resize uploaded profile photo.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Create your models here.
@@ -10,3 +11,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        image_to_save = Image.open(self.image.path)
+
+        if image_to_save.height > 300 or image_to_save.width > 300:
+            output_size = (300, 300)
+            image_to_save.thumbnail(output_size)
+            image_to_save.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,4 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html',)
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_update_form.is_valid():
+            user_form.save()
+            profile_update_form.save()
+            messages.success(request, f'Account updated')
+            return redirect('profile')  # Post/Redirect/Get pattern
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_update_form': profile_update_form
+    }
+    return render(request, 'users/profile.html', context)
